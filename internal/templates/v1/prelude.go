df103f677a2e5b21863cf4ab6612500f66ef1a30
Yaroslav Podorvanov <yaroslav.podorvanov@gmail.com>
2024-03-23 03:11:10 +0200

    Connections from Ukraine

--- internal/templates/v1/prelude.go
+++ internal/templates/v1/prelude.go
@@ -10,7 +10,7 @@ import (
 
 type Company = domain.Company
 
-func linkedinConnections(companies []Company) string {
+func linkedinConnections(companies []Company, ukrainianUniversity bool) string {
 	ids := make([]string, len(companies))
 	for i, c := range companies {
 		for _, l := range c.LinkedInProfiles {
@@ -24,6 +24,10 @@ func linkedinConnections(companies []Company) string {
 		"currentCompany": {string(companyQueryParam)},
 		"network":        {`["F","S"]`},
 		"keywords":       {`"Go" OR "Golang"`},
+	}
+
+	if ukrainianUniversity {
+		values["schoolFilter"] = []string{`["364340","496320","782774","818029","850102","1257361","15099424","15099711","15101057","15101979","15102004","15250306"]`}
 	}
 
 	return "https://www.linkedin.com/search/results/PEOPLE/?" + values.Encode()
